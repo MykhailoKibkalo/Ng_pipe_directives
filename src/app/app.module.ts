dfbc4a1b040bc4f266cd28a51b08d505de0e6fde
Liza <yelyzavetalog@gmail.com>
2021-02-08 15:09:10 +0200

    less8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CustomPipePipe } from './pipe/custom-pipe.pipe';
-import { RedDirective } from './directives/red.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomPipePipe,
-    RedDirective
   ],
   imports: [
     BrowserModule
