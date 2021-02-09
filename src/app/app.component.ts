d4b38b433f32c3b3d63c6d21de298e518c42215f
Liza <yelyzavetalog@gmail.com>
2021-02-09 17:53:48 +0200

    cw09-02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My Angular Project is very good';
-  asyncValue = new Promise(resolve => {
-    setTimeout( () => {
-      resolve ('It is example async pipe');
-    }, 1000);
-  });
-
-  date = new Date();
-  price = 100;
-  e = 2.742462734682743987;
-  user = {name: 'Ivan', age: 22, status: true};
 }
